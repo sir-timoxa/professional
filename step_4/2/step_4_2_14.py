b8f8e5ffbccd92895f41cd154c2fcdee03d90356
Timashkov Anton <fc_cometa@mail.ru>
2023-03-09 18:10:12 +0300

    some step done

--- step_4/2/step_4_2_14.py
+++ step_4/2/step_4_2_14.py
@@ -1,5 +1,14 @@
 import csv
 
-with open('deniro.csv', encoding='utf-8') as file:
-    rows = csv.DictReader(file, delimiter=';')
+column=int(input())
 
+with open('deniro.csv', encoding='utf-8') as file:
+    rows = list(csv.reader(file, delimiter=','))
+    column-=1
+    if column==0:
+        result=sorted(rows,key=lambda x: x[0])
+    else:
+        result=sorted(rows,key=lambda x: int(x[column]))
+    
+for x in result:
+    print(','.join(x))
---
+++ step_4/2/step_4_2_15.py
@@ -0,0 +1,12 @@
+import csv 
+
+
+def csv_columns(filename):
+    with open(filename, encoding='utf-8') as file:
+        rows = csv.DictReader(file, delimiter=',')
+        result={}
+        for dictionary in rows:
+            for key,val in dictionary.items():
+                result.setdefault(key,[]).append(val)
+
+    return result
