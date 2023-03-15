0fc17c708c865881f548d0fd39486c8805c25e96
Timashkov Anton <fc_cometa@mail.ru>
2023-03-15 17:03:48 +0300

    step_4_1 done

--- step_4/2/step_4_2_22.py
+++ step_4/2/step_4_2_22.py
@@ -4,14 +4,11 @@ with open('prices.csv', encoding='utf-8') as f:
     rows = csv.DictReader(f, delimiter=';')
     result={}
     for row in rows:
-        print(row)
-
+        magaz_name=row.pop('Магазин')
+        # print(row)
         # print(min(row.items(),key=lambda x: int(x[1])))
-
- 
-
-
-        # result[row['Магазин']]=result.setdefault(row)
-
-
-
+        result.setdefault(magaz_name,min(row.items(),key=lambda x: int(x[1])))
+    # print(result)
+    final = min(result.items(),key=lambda x: int(x[1][1]))
+    # print(final)
+    print(f"{final[1][0]} : {final[0]}")
