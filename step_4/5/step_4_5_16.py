52614e99edcdf07e569a1fea321ab43dac510678
Timashkov Anton <fc_cometa@mail.ru>
2023-04-13 14:46:19 +0300

    step_4_5_16 done

---
+++ step_4/5/step_4_5_16.py
@@ -0,0 +1,14 @@
+from zipfile import ZipFile
+
+with ZipFile('workbook(1).zip') as zip_file:
+    info = zip_file.infolist()
+    last = 100
+    for x in info:
+        if x.is_dir():
+            continue
+        else:
+            if (x.compress_size/x.file_size)*100 <= last:
+                last = (x.compress_size/x.file_size)*100
+                last_file=x.filename
+print(last_file.split('/')[-1])
+
