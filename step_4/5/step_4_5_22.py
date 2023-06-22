1bb6c0e219a415bf5dbfb821af7b374371ca4449
Anton Timashkov <atimashkov@ptsecurity.com>
2023-06-22 18:05:20 +0300

    start step_4_5_22

--- step_4/5/step_4_5_22.py
+++ step_4/5/step_4_5_22.py
@@ -1,11 +1,24 @@
 from zipfile import ZipFile
+import json
 
 with ZipFile('data.zip') as zip_file:
     info = zip_file.infolist()
     for path in info:
         if path.filename.endswith('.json'):
+            # print(path.filename)
             try:
+                result=[]
                 with zip_file.open(path.filename) as file:
-                    print(file.read().decode('utf-8'))
+                    # print(file.read().decode('utf-8'))
+                    my_data=json.load(file)
+                    if my_data['team']=='Arsenal':
+                            result.append([my_data['first_name'],my_data['last_name']])
+                    # print(my_data)
+                #     for team in my_data:
+                #         print(team)
+                #         if team['team']=='Arsenal':
+                #             result.append([team['first_name'],team['last_name']])
+                result=sorted(result,key=)
+                print(*result)
             except:
                 pass
