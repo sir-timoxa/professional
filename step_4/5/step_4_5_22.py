2fb5b258693bb49a8e10eb3f11321500d90afdaa
Anton Timashkov <fc_cometa@mail.ru>
2023-06-26 15:04:07 +0300

    step_4_5_22 done

--- step_4/5/step_4_5_22.py
+++ step_4/5/step_4_5_22.py
@@ -1,24 +1,16 @@
 from zipfile import ZipFile
 import json
-
+result=[]
 with ZipFile('data.zip') as zip_file:
     info = zip_file.infolist()
     for path in info:
         if path.filename.endswith('.json'):
-            # print(path.filename)
             try:
-                result=[]
                 with zip_file.open(path.filename) as file:
-                    # print(file.read().decode('utf-8'))
                     my_data=json.load(file)
                     if my_data['team']=='Arsenal':
-                            result.append([my_data['first_name'],my_data['last_name']])
-                    # print(my_data)
-                #     for team in my_data:
-                #         print(team)
-                #         if team['team']=='Arsenal':
-                #             result.append([team['first_name'],team['last_name']])
-                result=sorted(result,key=)
-                print(*result)
+                        result.append(f"{my_data['first_name']} {my_data['last_name']}")
             except:
                 pass
+result.sort()
+print(*result,sep='\n')
