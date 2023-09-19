5926561067f8efd0173455ccd5e04bcfe31ecb6a
Timashkov Anton <fc_cometa@mail.ru>
2023-09-19 19:11:38 +0300

    some chamges

--- step_4/6/step_4_6_15.py
+++ step_4/6/step_4_6_15.py
@@ -5,13 +5,18 @@ hashsumm=int(input())
 
 with open(filename, mode='rb') as file:
     data=pickle.load(file)
-    if type(data) is dict:
-        data_hash=sum(list(filter(lambda x: type(x) is int, data)))
-    elif type(data) is list:
-        reb=list(filter(lambda x: type(x) is int,data))
-        data_hash= reb[0]*reb[1]
-    if data_hash==hashsumm:
-        print('Контрольные суммы совпадают')
+
+if type(data) is dict:
+    data_hash=sum(list(filter(lambda x: type(x) is int, data)))
+elif type(data) is list:
+    reb=list(filter(lambda x: type(x) is int,data))
+    if len(reb)<2:
+        data_hash=0
     else:
-        print('Контрольные суммы не совпадают')
+        data_hash= min(reb)*max(reb)
+
+if data_hash==hashsumm:
+    print('Контрольные суммы совпадают')
+else:
+     print('Контрольные суммы не совпадают')
 
