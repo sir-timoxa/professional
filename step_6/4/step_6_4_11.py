24e72b295de979b540342b2a535b828fdc900b6e
Timashkov Anton <fc_cometa@mail.ru>
2023-09-24 22:03:46 +0300

    step_6_4_11 done

---
+++ step_6/4/step_6_4_11.py
@@ -0,0 +1,31 @@
+from collections import namedtuple
+
+User = namedtuple('User', ['name', 'surname', 'email', 'plan'])
+
+users = [User('Mary', 'Griffin', 'sonnen@yahoo.com', 'Basic'),
+         User('Brenda', 'Young', 'retoh@outlook.com', 'Silver'),
+         User('Kathleen', 'Lyons', 'balchen@att.net', 'Gold'),
+         User('Pamela', 'Hicks', 'corrada@sbcglobal.net', 'Silver'),
+         User('William', 'Townsend', 'kosact@verizon.net', 'Gold'),
+         User('Clayton', 'Morris', 'berserk@yahoo.com', 'Silver'),
+         User('Dorothy', 'Dennis', 'sequin@live.com', 'Gold'),
+         User('Tyler', 'Walker', 'noahb@comcast.net', 'Basic'),
+         User('Joseph', 'Moore', 'ylchang@sbcglobal.net', 'Silver'),
+         User('Kenneth', 'Richardson', 'tbusch@me.com', 'Bronze'),
+         User('Stephanie', 'Bush', 'neuffer@live.com', 'Gold'),
+         User('Gregory', 'Hughes', 'juliano@att.net', 'Basic'),
+         User('Tracy', 'Wallace', 'sblack@me.com', 'Silver'),
+         User('Russell', 'Smith', 'isaacson@comcast.net', 'Bronze'),
+         User('Megan', 'Patterson', 'hoangle@outlook.com', 'Basic')]
+
+
+sorted_order=['Gold','Silver','Bronze','Basic']
+
+
+sorted_users=sorted(users, key = lambda x: (sorted_order.index(x.plan), x.email))
+
+for x in sorted_users:
+    print(x.name,x.surname)
+    print(f'  Email: {x.email}')
+    print(f'  Plan: {x.plan}')
+    print()
