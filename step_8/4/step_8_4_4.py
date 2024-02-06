1bd7d7657ac78347f80dcb687fe5f20680bc0058
Anton Timashkov <atimashkov@ptsecurity.com>
2024-02-06 16:37:34 +0300

    step_8_4_4 done

---
+++ step_8/4/step_8_4_4.py
@@ -0,0 +1,12 @@
+def recursive_sum(data):
+    sum=0
+    for i in data:
+        if type(i) == int:
+            sum+=int(i) 
+        if type(i) == list:
+            sum += recursive_sum(i)
+    return sum
+
+my_list = [1, [4, 4], 2, [1, [2, 10]]]
+
+print(recursive_sum(my_list))
